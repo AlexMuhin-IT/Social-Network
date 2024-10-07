8c7c00f4cb51e9a71ebe8922c6306e8f496e5356
alexMuhin <myx87@bk.ru>
2024-10-07 20:00:31 +0300

    add className work is dialog

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,17 +5,17 @@ export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialog}>
-                <div className={s.dialog_user}>Limon</div>
-                <div className={s.dialog_user}>Dimon</div>
-                <div className={s.dialog_user}>Andpon</div>
-                <div className={s.dialog_user}>Pokimon</div>
-                <div className={s.dialog_user}>Doleshe Mon</div>
+                <div className={s.dialogUser}>Limon</div>
+                <div className={s.dialogUser}>Dimon</div>
+                <div className={s.dialogUser}>Andpon</div>
+                <div className={s.dialogUser}>Pokimon</div>
+                <div className={s.dialogUser}>Doleshe Mon</div>
             </div>
-                <div className={s.messages}>
-                    <div className={s.message}>'Hi how are you'</div>
-                    <div className={s.message}>'Hi, it"s okey'</div>
-                    <div className={s.message}>message</div>
-                </div>
+            <div className={s.messages}>
+                <div className={s.message}>'Hi how are you'</div>
+                <div className={s.message}>'Hi, it"s okey'</div>
+                <div className={s.message}>message</div>
+            </div>
         </div>
     )
 }
