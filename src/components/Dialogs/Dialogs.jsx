1d3dafbbd8da18670cdeacffdf12b0809f418732
alexMuhin <myx87@bk.ru>
2024-10-07 18:24:24 +0300

    add className work is dialog

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-// import s from './News.css';
+import s from './Dialogs.module.css';
 
-export const Dialogs = ()=>{
-    return(
-        <div>
-            Dialogs
+export const Dialogs = () => {
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialog}>
+                <div className={s.dialog_user}>Limon</div>
+                <div className={s.dialog_user}>Dimon</div>
+                <div className={s.dialog_user}>Andpon</div>
+                <div className={s.dialog_user}>Pokimon</div>
+                <div className={s.dialog_user}>Doleshe Mon</div>
+            </div>
+                <div className={s.messages}>
+                    <div className={s.message}>'Hi how are you'</div>
+                    <div className={s.message}>'Hi, it"s okey'</div>
+                    <div className={s.message}>message</div>
+                </div>
         </div>
     )
 }
