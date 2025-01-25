0f18de9dbc8b36db19d1e39d73ca12d9b04de82c
alexMuhin <myx87@bk.ru>
2025-01-25 13:25:51 +0300

    работаю над проектом

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import s from './Navbar.module.css'
 import {NavLink} from "react-router-dom";
+import {Friends} from "../Friends/Friendts";
+import {state} from "../../redux/state";
 
 
 export const Navbar = () => {
@@ -11,32 +13,61 @@ export const Navbar = () => {
                     <li className={s.item}>
                         <NavLink
                             to='/profile'
-                             className={({ isActive }) => (isActive ? s.active : s.item)}
+                            className={({isActive}) => (isActive ? s.active : s.item)}
                         >Profile</NavLink>
                     </li>
                     <li className={s.item}>
                         <NavLink
                             to='/dialogs'
-                             className={({ isActive }) => (isActive ? s.active : s.item)}
+                            className={({isActive}) => (isActive ? s.active : s.item)}
                         >Messages</NavLink>
                     </li>
                     <li className={s.item}>
                         <NavLink
                             to='/news'
-                             className={({ isActive }) => (isActive ? s.active : s.item)}
+                            className={({isActive}) => (isActive ? s.active : s.item)}
                         >News</NavLink>
                     </li>
                     <li className={s.item}>
                         <NavLink
                             to='/music'
-                             className={({ isActive }) => (isActive ? s.active : s.item)}
+                            className={({isActive}) => (isActive ? s.active : s.item)}
                         >Music</NavLink>
                     </li>
                     <li className={s.item}>
                         <NavLink
                             to='/settings'
-                             className={({ isActive }) => (isActive ? s.active : s.item)}
+                            className={({isActive}) => (isActive ? s.active : s.item)}
                         >Settings</NavLink>
+                    </li>
+                    <li className={s.item}>
+                        <NavLink
+                            to='/friends'
+                            className={({isActive}) => (isActive ? s.active : s.item)}
+                            // state={state.dialogPage}
+                        >Friends
+                            <div className={s.item_container}>
+                                {/*<Friends state={state.dialogPage} />*/}
+                                <div>
+                                    <div className={s.item_avatar}></div>
+                                    <div className={s.item_name}>Alex</div>
+                                </div>
+                                <div>
+                                    <div className={s.item_avatar}></div>
+                                    <div className={s.item_name}>Bob</div>
+                                </div>
+                                <div>
+                                    <div className={s.item_avatar}></div>
+                                    <div className={s.item_name}>Sasha</div>
+                                </div>
+
+                                {/*<div className={s.item_avatar}></div>*/}
+                                {/*<div className={s.item_name}>Bob</div>*/}
+                                {/*<div className={s.item_avatar}></div>*/}
+                                {/*<div className={s.item_name}>Sasha</div>*/}
+                            </div>
+
+                        </NavLink>
                     </li>
                 </ul>
             </div>
