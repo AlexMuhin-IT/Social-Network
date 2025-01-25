ac140d0a38be5dc538330c0f02ae81bb71106f55
alexMuhin <myx87@bk.ru>
2025-01-25 13:25:58 +0300

    работаю над проектом

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,15 +3,17 @@ import s from './Profile.module.css'
 import {MyPosts} from "./MyPosts/MyPosts";
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 import {BackGroundProject} from "./BackGroundProject/BackGroundProject";
+import {Outlet} from "react-router-dom";
+import {addPost} from "../../redux/state";
 
 
-export const Profile = () => {
-
+export const Profile = (props) => {
+debugger
     return (
         <div className={s.content__wrapper}>
             <BackGroundProject/>
             <ProfileInfo/>
-            <MyPosts/>
+            <MyPosts posts={props.state.posts} addPost={addPost}/>
         </div>
     )
 }
